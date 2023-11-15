b184bf73e9570ba9baa0c7c78ca3e2569c409fba
Moniaar <www.omniaahmed1562001@gmail.com>
2023-11-14 20:32:00 -0800

    this opcode bascially does'nt do anything

---
+++ 5-nop.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+ * nop_opcode - A function that bascally does nothing
+ *
+ * @stack: a pointer to a pointer pointing to top node of the stack
+ *
+ * @line_number: integer to tell you the line number of the given opCode
+ *
+ * Return: Void (Nothing)
+ */
+
+void nop_opcode(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
