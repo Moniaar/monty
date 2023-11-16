79d877473cb8ab78347357a25aaef7e2d670accf
Moniaar <www.omniaahmed1562001@gmail.com>
2023-11-16 06:36:19 -0800

    this function prints the stack from bottom to top

---
+++ rotTotop.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * rotLastTop - Rotates the last node of the stack to the top.
+ *
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ *
+ * @ln: Interger representing the line number of of the opcode.
+ */
+
+void rotLastTop(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+        stack_t *temp;
+
+        if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+                return;
+
+        temp = *stack;
+
+        while (temp->next != NULL)
+                temp = temp->next;
+
+        temp->next = *stack;
+        temp->prev->next = NULL;
+        temp->prev = NULL;
+        (*stack)->prev = temp;
+        (*stack) = temp;
+}
