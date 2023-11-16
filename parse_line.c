e8c28b1ae168e7c7f977472870dc31246d77eecd
Moniaar <www.omniaahmed1562001@gmail.com>
2023-11-16 06:26:10 -0800

    this file holds the function that sepreates given string into sections and modify what's inside of it

---
+++ parse_line.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+/**
+ * parseline - Separates each line into tokens to determine
+ * which function to call from the different ones in the lists below
+ * @buffer: line from the file
+ *
+ * @line_number: line number
+ *
+ * @format:  storage format. If 0 Nodes will be entered as a stack.
+ * if 1 nodes will be entered as a queue.
+ * Return: Returns 0 if the opcode is stack. 1 if queue.
+ *
+ */
+
+int parseline(char *buffer, int line_number, int format)
+{
+        char *opcd, *v;
+        const char *delim = "\n ";
+
+        if (buffer == NULL)
+                error_mesg(4);
+
+        opcd = strtok(buffer, delim);
+        if (opcd == NULL)
+                return (format);
+        v = strtok(NULL, delim);
+
+        if (strcmp(opcd, "stack") == 0)
+                return (0);
+        if (strcmp(opcd, "queue") == 0)
+                return (1);
+
+        findFuncs(opcd, v, line_number, format);
+        return (format);
+}
