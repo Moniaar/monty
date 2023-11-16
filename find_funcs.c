ab4045332d0bba5cfa025ab3bad67ea737dc3bd9
Moniaar <www.omniaahmed1562001@gmail.com>
2023-11-16 07:02:16 -0800

    this functions will go and search for the entered command between the functions provided in a list

--- find_funcs.c
+++ find_funcs.c
@@ -18,39 +18,39 @@
 
 void findFuncs(char *opcode, char *value, int ln, int format)
 {
-        int k;
-        int flag;
+	int k;
+	int flag;
 
-        instruction_t func_list[] = {
-                {"push", addnodeTos},
-                {"pall", pall},
-                {"pint", printTop},
-                {"pop", popTop},
-                {"nop", Nop},
-                {"swap", swapTop2elem},
-                {"add", addTOtop},
-                {"sub", subTotop},
-                {"div", divNodes},
-                {"mul", multiply_nodes},
-                {"mod", rem_nodes},
-                {"pchar", printAscii},
-                {"pstr", printString},
-                {"rotl", rotateFtoBot},
-                {"rotr", rotLastTop},
-                {NULL, NULL}
-        };
+	instruction_t func_list[] = {
+		{"push", addnodeTos},
+		{"pall", pall},
+		{"pint", printTop},
+		{"pop", popTop},
+		{"nop", Nop},
+		{"swap", swapTop2elem},
+		{"add", addTOtop},
+		{"sub", subTotop},
+		{"div", divNodes},
+		{"mul", multiply_nodes},
+		{"mod", rem_nodes},
+		{"pchar", printAscii},
+		{"pstr", printString},
+		{"rotl", rotateFtoBot},
+		{"rotr", rotLastTop},
+		{NULL, NULL}
+	};
 
-        if (opcode[0] == '#')
-                return;
+	if (opcode[0] == '#')
+		return;
 
-        for (flag = 1, k = 0; func_list[k].opcode != NULL; k++)
-        {
-                if (strcmp(opcode, func_list[k].opcode) == 0)
-                {
-                        callfuncs(func_list[k].f, opcode, value, ln, format);
-                        flag = 0;
-                }
-        }
-        if (flag == 1)
-                error_mesg(3, ln, opcode);
+	for (flag = 1, k = 0; func_list[k].opcode != NULL; k++)
+	{
+		if (strcmp(opcode, func_list[k].opcode) == 0)
+		{
+			callfuncs(func_list[k].f, opcode, value, ln, format);
+			flag = 0;
+		}
+	}
+	if (flag == 1)
+		error_mesg(3, ln, opcode);
 }
