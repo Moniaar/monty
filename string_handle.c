8af0d4397e6bcd422ed64462f168b9207ad474aa
Moniaar <www.omniaahmed1562001@gmail.com>
2023-11-16 06:19:23 -0800

    this file holds the function that will print a string given in a stack

--- string_handle.c
+++ string_handle.c
@@ -1,27 +1,7 @@
 #include "monty.h"
 
 /**
- * printAscii - Prints the Ascii value.
- *
- * @stack: Pointer to a pointer pointing to top node of the stack.
- *
- * @line_number: Interger representing the line number of of the opcode.
- */
-void printAscii(stack_t **stack, unsigned int line_number)
-{
-	int a;
-
-	if (stack == NULL || *stack == NULL)
-		stringErr(11, line_number);
-
-	a = (*stack)->n;
-	if (a < 0 || a > 127)
-		stringErr(10, line_number);
-	printf("%c\n", a);
-}
-
-/**
- * printString - Prints a string.
+ * printString - Prints a string given by the monty script
  *
  * @stack: Pointer to a pointer pointing to top node of the stack.
  *
@@ -49,58 +29,4 @@ void printString(stack_t **stack, __attribute__((unused))unsigned int ln)
 		temp = temp->next;
 	}
 	printf("\n");
-}
-
-/**
- * rotateFtoBot - Rotates the first node of the stack to the bottom.
- *
- * @stack: Pointer to a pointer pointing to top node of the stack.
- *
- * @ln: Interger representing the line number of of the opcode.
- */
-
-void rotateFtoBot(stack_t **stack, __attribute__((unused))unsigned int ln)
-{
-	stack_t *temp;
-
-	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-		return;
-
-	temp = *stack;
-	while (temp->next != NULL)
-		temp = temp->next;
-
-	temp->next = *stack;
-	(*stack)->prev = temp;
-	*stack = (*stack)->next;
-	(*stack)->prev->next = NULL;
-	(*stack)->prev = NULL;
-}
-
-
-/**
- * rotLastTop - Rotates the last node of the stack to the top.
- *
- * @stack: Pointer to a pointer pointing to top node of the stack.
- *
- * @ln: Interger representing the line number of of the opcode.
- */
-
-void rotLastTop(stack_t **stack, __attribute__((unused))unsigned int ln)
-{
-	stack_t *temp;
-
-	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-		return;
-
-	temp = *stack;
-
-	while (temp->next != NULL)
-		temp = temp->next;
-
-	temp->next = *stack;
-	temp->prev->next = NULL;
-	temp->prev = NULL;
-	(*stack)->prev = temp;
-	(*stack) = temp;
 }
