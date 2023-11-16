d2cb507873de67c695f2e69f0530b308a1bed48d
Moniaar <www.omniaahmed1562001@gmail.com>
2023-11-16 07:15:02 -0800

    This contains the errors that will come by you adding different command than whats expected or nothing at all

--- stringErr.c
+++ stringErr.c
@@ -12,22 +12,22 @@
 
 void stringErr(int error_num, ...)
 {
-        va_list args;
-        int l_num;
+	va_list args;
+	int l_num;
 
-        va_start(args, error_num);
-        l_num = va_arg(args, int);
-        switch (error_num)
-        {
-                case 10:
-                        fprintf(stderr, "L%d: can't pchar, value out of range\n", l_num);
-                        break;
-                case 11:
-                        fprintf(stderr, "L%d: can't pchar, stack empty\n", l_num);
-                        break;
-                default:
-                        break;
-        }
-        freeNodes();
-        exit(EXIT_FAILURE);
+	va_start(args, error_num);
+	l_num = va_arg(args, int);
+	switch (error_num)
+	{
+		case 10:
+			fprintf(stderr, "L%d: can't pchar, value out of range\n", l_num);
+			break;
+		case 11:
+			fprintf(stderr, "L%d: can't pchar, stack empty\n", l_num);
+			break;
+		default:
+			break;
+	}
+	freeNodes();
+	exit(EXIT_FAILURE);
 }
